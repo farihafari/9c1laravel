63a166fdc28be359b1b416358652fb96b81c54ad
farihafari <fareehajabeen62@gmail.com>
2024-11-07 13:01:42 +0500

    controller

---
+++ resources/views/panel/category.blade.php
@@ -0,0 +1,24 @@
+@include ("components.header")
+
+<div class="container-fluid pt-4 px-4">
+                <div class="row bg-light rounded mx-0">
+                    <div class="col-md-12">
+                        <h3>categories</h3>
+                        <form method="post" enctype="multipart/form-data">
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">name</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" name="cname"
+                                        aria-describedby="emailHelp">
+                                    
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">image</label>
+                                    <input type="file" name="cimage" class="form-control" id="exampleInputPassword1">
+                                </div>
+                             
+                                <button type="submit" class="btn btn-primary">submit</button>
+                            </form>
+                    </div>
+                </div>
+            </div>
+@include ("components.footer")
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('panel.index');
 });
-// Route::get("home",function(){
-//     return view("index");
-// });
+Route::get("categories",function(){
+    return view("panel.category");
+});
